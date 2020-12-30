b39360af026752311555573dc0573f513e727cd4
Erin Winking <ewink@yahoo.com>
2020-12-30 14:06:36 -0800

    Validation function completed, including age check.

--- Login_And_Registration/models.py
+++ Login_And_Registration/models.py
@@ -1,16 +1,34 @@
 from django.db import models
-import re
+import re, datetime
+from datetime import date, datetime
+
+# Thanks, StackOverflow!
+def calculate_age(born):
+    today = date.today()
+    return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
 
 class ValidationTest(models.Manager):
     def basic_validation(self, postData):
         errors = {}
-        if len(postData['first_name']) < 2:
-            errors['first_name'] = "First name must be more than two characters."
-        if len(postData['last_name']) < 2:
-            errors['last_name'] = "Last name must be more than two characters."
+        if len(postData['firstname']) < 2:
+            errors['firstname'] = "First name must be more than two characters."
+        if len(postData['lastname']) < 2:
+            errors['lastname'] = "Last name must be more than two characters."
         EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
         if not EMAIL_REGEX.match(postData['email']):
             errors['email'] = "Invalid email address!"
+        # Magic to make the date comparison work.
+        dateentered = postData['birthdate']
+        birthdate = datetime.strptime(dateentered, "%Y-%m-%d")
+        today = datetime.now()
+        if birthdate >= today:
+            errors['birthdate'] = "You're not a time traveler. Your birthdate must be in the past."
+        elif calculate_age(birthdate) < 13:
+            errors['birthdate'] = "You must be at least 13 years old to sign up for this website."
+        if len(postData['password']) < 8:
+            errors['password'] = "Your password must be at least 8 characters."
+        if not postData['confirm-pw'] == postData['password']:
+            errors['confirm-pw'] = "Password mismatch. Maybe a typo? Please try again!"
         return errors
 
 class User(models.Model):
--- Login_And_Registration/views.py
+++ Login_And_Registration/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render, redirect
+from django.http.response import HttpResponse
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
+from .models import User
 
 def home(request):
     return render(request, 'html/home.html' )
 
 def register(request):
-    pass
+    errors = User.objects.basic_validation(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value, extra_tags='danger')
+        return redirect('home')
+    return HttpResponse('PASSED')
 
 def login(request):
     pass
